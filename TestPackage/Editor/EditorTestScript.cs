e7c10dc1968bd6bc0d5803d1a0ee845dc89abf93
ChrisLivingBrain <christian.gavenea@livingbrain.de>
2021-02-22 15:56:49 +0100

    ASD

---
+++ TestPackage/Editor/EditorTestScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    public class EditorTestScript
+    {
+        public static void StaticTest()
+        {
+            
+        }
+
+        public void NonStaticTest()
+        {
+            
+        }
+    }
---
+++ TestPackage/Runtime/PackageRuntimeTestScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PackageRuntimeTestScript
+{
+    public static void StaticTest()
+    { 
+
+    }
+
+    public void NonStaticTest()
+    {
+
+    }
+}
